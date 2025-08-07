edddefade5c6df99193439d8dbdbb866ef8aaacb
PK <125938003+PeakANK@users.noreply.github.com>
2025-08-07 23:18:41 +0700

    Add nestJS/config and test port

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // ✅ this makes env variables accessible everywhere
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
